24aa16eb91dff2d698d3b5e071a167327e54e9df
Gabriel Santana <gabrielsantana7791@gmail.com>
2026-05-14 11:25:43 -0300

    Revert "removed var env from config.ts"
    This reverts commit 41327671576f06cf01bf63c2d83b9ebc190858cc.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,14 +2,14 @@ import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
 import { Auth, getAuth } from "firebase/auth";
 
-// This is NOT private
+// This is NOT private keys
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyDfKKtyAXjFrHnLa364LjmwkU4DPl0YdSM",
-  authDomain: "patinhas-para-adocao.firebaseapp.com",
-  projectId: "patinhas-para-adocao",
-  storageBucket: "patinhas-para-adocao.firebasestorage.app",
-  messagingSenderId: "379260877007",
-  appId: "1:379260877007:web:358604a96fc436de40dfba",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 let app: FirebaseApp;
